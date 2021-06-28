122eba5887ab029c195a4dd52623c4a57b12fc4c
HiromuKinoshita <uw.p.21k@gmail.com>
2021-06-28 10:02:49 +0900

    add zoom tab close function and comment out others

--- popup.js
+++ popup.js
@@ -1,24 +1,35 @@
+// TODO: delete them
 // Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
+// let changeColor = document.getElementById("changeColor");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+// chrome.storage.sync.get("color", ({ color }) => {
+//   changeColor.style.backgroundColor = color;
+// });
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+removeZoomTabc.addEventListener("click", async () => {
+  const tabs = await chrome.tabs.query({
+    /**
+     * Target should be inactive tabs.
+     * A user may want to reopen zoom app via browser if tab is active.
+     */
+    active: false,
+    url: [
+      'https://zoom.us/*#success',
+      'https://*.zoom.us/*#success',
+      'https://zoom.us/postattendee*',
+      'https://*.zoom.us/postattendee*',
+    ],
+  });
+  tabs.forEach(tab => {
+    chrome.tabs.remove(tab.id);
   });
 });
 
+// TODO: delete them
 // The body of this function will be executed as a content script inside the
 // current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
-}
+// function setPageBackgroundColor() {
+//   chrome.storage.sync.get("color", ({ color }) => {
+//     document.body.style.backgroundColor = color;
+//   });
+// }
