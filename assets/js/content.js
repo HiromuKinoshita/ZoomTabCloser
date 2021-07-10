5cde6569f6c209bcacc26c709ff5b4b1384653c3
HiromuKinoshita <uw.p.21k@gmail.com>
2021-07-10 11:16:12 +0900

    rename files

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,3 +1,5 @@
+import removeZoomTabs from './remove';
+
 // TODO: delete them
 // Initialize button with user's preferred color
 // let changeColor = document.getElementById("changeColor");
@@ -6,24 +8,6 @@
 //   changeColor.style.backgroundColor = color;
 // });
 
-const removeZoomTabs = async () => {
-  const tabs = await chrome.tabs.query({
-    /**
-     * Target should be inactive tabs.
-     * A user may want to reopen zoom app via browser if tab is active.
-     */
-    active: false,
-    url: [
-      'https://zoom.us/*#success',
-      'https://*.zoom.us/*#success',
-      'https://zoom.us/postattendee*',
-      'https://*.zoom.us/postattendee*',
-    ],
-  });
-  tabs.forEach(async tab => {
-    chrome.tabs.remove(tab.id);
-  });
-}
 
 buttonRemove.addEventListener("click", removeZoomTabs());
 
