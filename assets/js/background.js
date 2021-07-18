3ded2e666e48171b268faee2389c6523a3db4f3e
HiromuKinoshita <uw.p.21k@gmail.com>
2021-07-18 22:43:30 +0900

    set periodical processing on install

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,8 +1,27 @@
 import { removeZoomTabs } from './remove';
 
-let color = '#3aa757';
+const basisMinutesOptions = [...Array(60).keys()].map(i => ++i);
+const intervalMinutesOptions = [15, 30, 60];
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+const basisMinutes = basisMinutesOptions[5];
+const intervalMinutes = intervalMinutesOptions[0];
+
+const currentMinutes = new Date().getMinutes();
+
+let nextEventMinutes = basisMinutes;
+while (currentMinutes > nextEventMinutes) {
+  nextEventMinutes += intervalMinutes;
+}
+const untillNextEventMinutes = nextEventMinutes - currentMinutes;
+const currentTime = Date.now();
+
+chrome.alarms.create('removeTab', {
+  periodInMinutes: intervalMinutes,
+  when: (currentTime + untillNextEventMinutes * 60) * 1000,
+});
+
+chrome.alarms.onAlarm.addListener(alarm => {
+  console.log(alarm);
+  console.log(new Date().getTime());
+  removeZoomTabs();
 });
