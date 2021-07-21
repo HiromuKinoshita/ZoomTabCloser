74dd93259989f07d4d4bc7a176d36344bf9c10ae
HiromuKinoshita <uw.p.21k@gmail.com>
2021-07-21 17:56:33 +0900

    re-create event on setting is changed

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,11 +1,35 @@
 import { removeZoomTabs } from './remove';
-import { getIntervalMinutes, firstEventUnixTime } from './schedule'
+import { firstEventUnixTime } from './schedule'
 
-chrome.alarms.create('removeTab', {
-  periodInMinutes: getIntervalMinutes(),
-  when: firstEventUnixTime(),
-});
+const set = interval => {
+  const time = firstEventUnixTime(interval);
+  chrome.alarms.create('removeTab', {
+    periodInMinutes: interval,
+    when: time,
+  })
+};
+
+chrome.storage.sync.get(
+  ['interval'],
+  async data => {
+    if (!data) {
+      // set initial value
+      await chrome.storage.sync.set({ interval: initialIntervalMinutes }, data => {
+        set(parseInt(data.interval));
+      });
+    } else {
+      set(parseInt(data.interval));
+    }
+  },
+);
 
 chrome.alarms.onAlarm.addListener(alarm => {
   removeZoomTabs();
+});
+
+chrome.storage.onChanged.addListener( async (changes) => {
+  await chrome.alarms.clearAll();
+  for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+    set(parseInt(newValue));
+  }
 });
--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -1,22 +1,17 @@
-import { intervalMinutesOptions } from './consts'
-
-const intervalMinutes = intervalMinutesOptions[0];
-
-const getUntillNextEventMinutes = () => {
+const getUntillNextEventMinutes = interval => {
   const currentMinutes = new Date().getMinutes();
   let nextEventMinutes = 0;
 
   while (currentMinutes > nextEventMinutes) {
-    nextEventMinutes += intervalMinutes;
+    nextEventMinutes += interval;
   }
 
   return nextEventMinutes - currentMinutes;
 };
 
-export const getIntervalMinutes = () => intervalMinutes;
-export const firstEventUnixTime = () => {
+export const firstEventUnixTime = interval => {
   const currentTime = Date.now();
-  const untillNextEventMinutes = getUntillNextEventMinutes();
+  const untillNextEventMinutes = getUntillNextEventMinutes(interval);
   // add minutes as milli seconds
   return currentTime + untillNextEventMinutes * 60 * 1000;
 };
