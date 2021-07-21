4a5a93957f2f610174577a19c0c86fe1ae00d2b3
HiromuKinoshita <uw.p.21k@gmail.com>
2021-07-21 17:29:10 +0900

    refine option.js

--- assets/js/options.js
+++ assets/js/options.js
@@ -1,46 +1,43 @@
-let page = document.getElementById("buttonDiv");
-let selectedClassName = "current";
-const presetButtonColors = ["#3aa757", "#e8453c", "#f9bb2d", "#4688f1"];
+import { intervalMinutesOptions, initialIntervalMinutes } from './consts'
 
-// Reacts to a button click by marking the selected button and saving
-// the selection
-function handleButtonClick(event) {
-  // Remove styling from the previously selected color
-  let current = event.target.parentElement.querySelector(
-    `.${selectedClassName}`
-  );
-  if (current && current !== event.target) {
-    current.classList.remove(selectedClassName);
-  }
+const button = document.getElementById("buttonSave");
+const select = document.getElementById("selectInterval");
 
-  // Mark the button as selected
-  let color = event.target.dataset.color;
-  event.target.classList.add(selectedClassName);
-  chrome.storage.sync.set({ color });
+const handleButtonClick = async () => {
+  await chrome.storage.sync.set({ interval: select.value });
 }
 
-// Add a button to the page for each supplied color
-function constructOptions(buttonColors) {
-  chrome.storage.sync.get("color", (data) => {
-    let currentColor = data.color;
-    // For each color we were provided…
-    for (let buttonColor of buttonColors) {
-      // …create a button with that color…
-      let button = document.createElement("button");
-      button.dataset.color = buttonColor;
-      button.style.backgroundColor = buttonColor;
-
-      // …mark the currently selected color…
-      if (buttonColor === currentColor) {
-        button.classList.add(selectedClassName);
+const constructOptions = async () => {
+  await chrome.storage.sync.get(
+    ['interval'],
+    async data => {
+      if (!data) {
+        // set initial value
+        await chrome.storage.sync.set({ interval: initialIntervalMinutes }, data => {
+          setOptionDom(data.interval);
+        });
+      } else {
+        setOptionDom(data.interval);
       }
+    },
+  );
 
-      // …and register a listener for when that button is clicked
-      button.addEventListener("click", handleButtonClick);
-      page.appendChild(button);
-    }
-  });
+  button.addEventListener("click", handleButtonClick);
 }
 
-// Initialize the page by constructing the color options
-constructOptions(presetButtonColors);
+const setOptionDom = currentInterval => {
+  for (const option of intervalMinutesOptions) {
+    const optionElement = document.createElement("option");
+    optionElement.value = option;
+    optionElement.innerText = option;
+
+    if (optionElement.value === currentInterval) {
+      select.value = optionElement.value;
+    }
+
+    select.appendChild(optionElement);
+  }
+}
+
+// Initialize
+constructOptions(intervalMinutesOptions);
