81dbcdee073fada80cbab7bcad75b93094859647
HiromuKinoshita <uw.p.21k@gmail.com>
2021-07-21 17:46:07 +0900

    remove comment

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,21 +1,3 @@
 import { removeZoomTabs } from './remove';
 
-// TODO: delete them
-// Initialize button with user's preferred color
-// let changeColor = document.getElementById("changeColor");
-
-// chrome.storage.sync.get("color", ({ color }) => {
-//   changeColor.style.backgroundColor = color;
-// });
-
-
 buttonRemove.addEventListener("click", removeZoomTabs);
-
-// TODO: delete them
-// The body of this function will be executed as a content script inside the
-// current page
-// function setPageBackgroundColor() {
-//   chrome.storage.sync.get("color", ({ color }) => {
-//     document.body.style.backgroundColor = color;
-//   });
-// }
