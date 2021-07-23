38ef7dabe56d28201252828f53388b63ac532c5a
HiromuKinoshita <uw.p.21k@gmail.com>
2021-07-23 17:01:03 +0900

    add link to setting

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,3 +1,10 @@
 import { removeZoomTabs } from './remove';
 
 buttonRemove.addEventListener("click", removeZoomTabs);
+
+chrome.management.getSelf(info => {
+  const link = document.getElementById('linkSetting');
+  console.log(link);
+  console.log(document);
+  link.href = `chrome-extension://${info.id}/options.html`;
+});
